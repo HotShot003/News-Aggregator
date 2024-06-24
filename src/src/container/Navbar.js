6b9096c48b0d5fe8900a4b8e2e2c20c5c1b215d6
Shreya <pshreya0711@gmail.com>
2024-06-24 22:16:27 +0530

    Fixed Navbar and Footer.

--- src/src/container/Navbar.js
+++ src/src/container/Navbar.js
@@ -5,17 +5,20 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Navbar = () => {
     return (
-        <nav className="py-4 bg-white flex items-center justify-between px-4 sm:px-8">
-            <img src={logo} alt="Logo" className="w-32 h-auto sm:w-44 sm:h-14 ml-2"/>
-            <div className="flex-1 mx-4 sm:mx-8 text-center">
-                <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">NewsHive</h1>
-            </div>
-            <div className="mr-2 sm:mr-4">
-                <div className="w-10 h-10 sm:w-12 sm:h-12 rounded-full overflow-hidden border-2 border-gray-300">
-                    <img src={profileImage} alt="Profile" className="w-full h-full object-cover"/>
+        <div>
+            <nav className="navbar py-4 flex items-center justify-between px-4 sm:px-8">
+                <img src={logo} alt="Logo" className="w-32 h-auto sm:w-44 sm:h-14 ml-2"/>
+                <div className="flex-1 mx-4 sm:mx-8 text-center">
+                    <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">NewsHive</h1>
                 </div>
-            </div>
-        </nav>
+                <div className="mr-2 sm:mr-4">
+                    <div className="w-10 h-10 sm:w-12 sm:h-12 rounded-full overflow-hidden border-2 border-gray-300">
+                        <img src={profileImage} alt="Profile" className="w-full h-full object-cover"/>
+                    </div>
+                </div>
+            </nav>
+            <hr className="hr-custom"/>
+        </div>
     );
 }
 
