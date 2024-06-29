0f9ed44e6574f5bee5011c48f967bde3f5de307e
Shreya <pshreya0711@gmail.com>
2024-06-30 01:55:49 +0530

    Integrate the Contact component and also done with routing.

--- src/src/App.js
+++ src/src/App.js
@@ -1,68 +1,34 @@
+// App.js
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './container/Navbar';
-import Content from './container/Content'; 
+import Content from './container/Content';
 import Dashboard from './container/Dashboard';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import Contact from './container/ContactUs/Contact'; // Import Contact component
 import Footer from './container/Footer';
-// import Contact from './container/ContactUs/Contact'; 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
+    <Router>
+      <div className="App">
+        <Navbar />
+        <hr className="hr-custom" />
         <div className="main-container">
           <aside className="sidebar">
             <Content />
           </aside>
           <main className="main-content">
-            <Dashboard />
-            {/* <Contact /> */}
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
           </main>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
 export default App;
-
-
-
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Navbar from './container/Navbar';
-// import Content from './container/Content';
-// import Dashboard from './container/Dashboard';
-// import LikedArticles from './container/LikedArticles';
-// import { LikedArticlesProvider } from './container/LikedArticlesContext';
-// import Footer from '../src/container/Footer';
-
-// function App() {
-//   return (
-//     <Router>
-//       <LikedArticlesProvider>
-//         <div className="App">
-//           <Navbar />
-//           <hr className="hr-custom" />
-//           <div className="main-container">
-//             <aside className="sidebar">
-//               <Content />
-//             </aside>
-//             <main className="main-content">
-//               <Switch>
-//                 <Route exact path="/" component={Dashboard} />
-//                 <Route path="/liked" component={LikedArticles} />
-//               </Switch>
-//             </main>
-//           </div>
-//           <Footer />
-//         </div>
-//       </LikedArticlesProvider>
-//     </Router>
-//   );
-// }
-
-// export default App;
